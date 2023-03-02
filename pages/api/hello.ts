fb11400f0a2553b6a59d44414ae8ea5b28f875fe
hexaalif <hexaalif2020@gmail.com>
2023-03-02 20:45:18 +0600

    jwt added and worked on it

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import jwt from "jsonwebtoken"
 
-type Data = {
-  name: string
-}
+const KEY= 'skfjeogjeroenkjrlergg';
+
+// type Data = {
+//   name: string
+// }
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+
+  if(!req.body){
+    res.statusCode = 404
+    res.end("Error")
+    return
+  }
+
+  const {username, password} = req.body
+
+  res.json({
+    token: jwt.sign({
+      username,
+      admin: username === 'admin' && password === 'admin'
+    }, KEY)
+  })
+
+  console.log( req.body);
+
+  // res.status(200).json({ name: 'John Doe' })
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
-
+import { useState } from "react";
+import styles from "../styles/index.module.css";
+import jwt from "jsonwebtoken";
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -15,24 +17,73 @@
 //   </div>
 // }
 
-
 export default function Home() {
-  const variables = Math.random() > 0.5 ? 'red': 'blue'
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [message, setMessage] = useState("you are not logged in");
+
+  async function submitForm() {
+    const res = await fetch("/api/hello", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    }).then((t) => t.json());
+
+    const token = res.token;
+
+    if (token) {
+      const json = jwt.decode(token);
+      setMessage(`welcom bro`);
+    } else {
+      setMessage("went wrong");
+    }
+  }
+
+  const variables = Math.random() > 0.5 ? "red" : "blue";
   return (
     <>
-    <style jsx>
-      {
-        `
-        .title{
-          color: ${variables};
-        }
-        `
-      }
-    </style>
+      <style jsx>
+        {`
+          .title {
+            color: ${variables};
+          }
+        `}
+      </style>
       <div>
-    {/* <Heading heading="Heading"/> */}
-        
-        <h1 className='title'>Hello World</h1></div>
+        <h1>{message}</h1>
+        {/* <Heading heading="Heading"/> */}
+        <p className={styles.sub}>I am Next</p>
+        <h1 className="title">Hello World</h1>
+
+        {/* Form part starts */}
+
+        <div>
+          <form method="POST" action="/api/hello">
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              id=""
+            />
+            <br />
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              id=""
+            />
+            <br />
+            <input type="button" value="login" onClick={submitForm} />
+          </form>
+        </div>
+
+        {/* Form part ends */}
+      </div>
     </>
-  )
+  );
 }
